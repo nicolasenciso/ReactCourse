a8ac8657e05bed9ca2ed988c806cefd2ae2157ee
nicolasenciso <nricardoe@unal.edu.co>
2021-02-10 16:22:48 -0500

    turning the default create react app to course style

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import React, { Component } from 'react';
+
+/* default
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi, im a react app</h1>
+    </div>
+  );
+}
+*/
+
+/*course*/
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>Hi im react ddd</h1>
+        <p>soy nicolas</p>
+      </div>
+    );
+  }
+}
+
+
+export default App;
